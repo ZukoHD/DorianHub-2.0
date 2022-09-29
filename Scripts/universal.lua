677eaac7d3c0b44f14278bdff8eaba6bc3a93d9f
dori <69031971+ZukoHD@users.noreply.github.com>
2022-09-29 17:28:53 +1000

    Update universal.lua

--- Scripts/universal.lua
+++ Scripts/universal.lua
@@ -1,3 +1,6 @@
+local Player = game:GetService("Players").LocalPlayer
+local Character = Player.Character
+
 local vre = "2.0"
 local theme = {
 	SchemeColor = Color3.fromRGB(100, 0, 255),
@@ -9,3 +12,21 @@ local theme = {
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Universal "..vre.." - Dorian Hub", theme)
+
+local Movement = Window:NewTab("Movement")
+local Visuals = Window:NewTab("Visuals")
+local Trolling = Window:NewTab("Trolling")
+local Credits = Window:NewTab("Credits")
+
+--// Movement Tabs
+local LocalPlayer = Movement:NewSection("LocalPlayer")
+local Humanoid = Movement:NewSection("Other")
+
+LocalPlayer:NewSlider("WalkSpeed", "Changes your WalkSpeed!", 500, 16, function(s)
+	Character.Humanoid.WalkSpeed = s
+end)
+
+LocalPlayer:NewSlider("JumpPower", "Changes your JumpPower!", 500, 16, function(s)
+	Character.Humanoid.JumpPower = s
+end)
+
