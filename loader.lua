35b280425a035a27ff79034970386b1ccf0e1168
dori <69031971+ZukoHD@users.noreply.github.com>
2022-09-29 18:48:20 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,14 @@
+local id = game.PlaceId
+local games = {
+	[606849621] = "https://raw.githubusercontent.com/ZukoHD/DorianHub-2.0/main/Scripts/jailbreak.lua"
+}
 
+local universal = "https://raw.githubusercontent.com/ZukoHD/DorianHub-2.0/main/Scripts/universal.lua"
+
+if games[id] then
+	local website = games[id]
+	
+	loadstring(game:HttpGet(website))()
+else
+	loadstring(game:HttpGet(universal))()
+end
