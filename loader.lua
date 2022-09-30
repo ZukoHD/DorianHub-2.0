8bd234e4912b39d80a322cec5157d7377b753354
dori <69031971+ZukoHD@users.noreply.github.com>
2022-09-30 18:43:10 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
 local games = { --// Add Games Here With Format: [GAME_ID] = "GITHUB SCRIPT LINK"
+	[155615604] = "https://raw.githubusercontent.com/ZukoHD/DorianHub-2.0/main/Scripts/prison_life.lua", --// Prison Life
 	[0] = "", --//
 }
 local exploit = "Unknown" if syn then exploit = 'Synapse X' elseif PROTOSMASHER_LOADED then exploit = 'Protosmasher' elseif issentinelclosure then exploit = 'Sentinel' elseif Krnl then exploit = 'Krnl' else game:GetService("Players").LocalPlayer:Kick("Unsupported Exploit!") end game:GetService("StarterGui"):SetCore("SendNotification", { Title = "Dorian Hub - Loaded",Text = "Dorian Hub has been loaded!",Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})Duration = 3 if games[game.PlaceId] then	loadstring(game:HttpGet(games[game.PlaceId]))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/ZukoHD/DorianHub-2.0/main/Scripts/universal.lua"))() end
